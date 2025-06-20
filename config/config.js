60257d7f4886ce38f810b60277e59a458a47706d
KeanuM123 <keanumoodley8@gmail.com>
2025-06-20 15:58:56 +0200

    feat: updated test setup and connected to remote DB

--- config/config.js
+++ config/config.js
@@ -1,19 +1,31 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+const isSSL = process.env.DB_SSL === 'true';
+
 const config = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
+    dialectOptions: isSSL
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME + '_test',
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
   },
   production: {
@@ -21,7 +33,16 @@ const config = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
+    dialectOptions: isSSL
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
   },
 };
 
