2890e697c79cd51e7c3b944777f13b561fb5a9dd
Sipo Kupiso <sipo.kupiso@capaciti.org.za>
2025-07-22 15:24:53 +0200

    Update ServiceList.jsx

--- client/src/components/ServiceList.jsx
+++ client/src/components/ServiceList.jsx
@@ -69,6 +69,7 @@ const ServiceList = () => {
                   fontSize: '0.9rem',
                   fontWeight: 'bold'
                 }}>
+                  {service.price ?'${service.price}':'Price TBD'}
                 </span>
               </div>
               
